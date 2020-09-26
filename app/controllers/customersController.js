4d138c9ca0f6e025c76e257b9d40dcb2c8453b11
Blessing Makaraba <blessingmakaraba@gmail.com>
2020-09-26 20:38:47 +0100

    Create global details with Values

--- app/controllers/customersController.js
+++ app/controllers/customersController.js
@@ -1,8 +1,9 @@
 (function () {
-  const CustomersController = function ($scope, customersService) {
+  const CustomersController = function ($scope, customersService, appSettings) {
     $scope.SortBy = "name";
     $scope.SortBy = false;
     $scope.customers = [];
+    $scope.appSettings = appSettings;
 
     function init() {
       $scope.customers = customersService.getCustomers();
@@ -15,7 +16,7 @@
     };
   };
 
-  CustomersController.$inject = ["$scope", "customersService"];
+  CustomersController.$inject = ["$scope", "customersService", "appSettings"];
 
   angular
     .module("customersApp")
